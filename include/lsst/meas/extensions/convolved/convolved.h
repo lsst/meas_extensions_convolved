63a145d2eec53e5b44398141754a3edec550065e
Paul Price <price@astro.princeton.edu>
2016-07-15 10:57:12 -0400

    add forced Kron on convolved image
    Feature requested by Furusawa. This is only good for forced measurement
    because if unforced then we don't know how large to make the convolved
    image (we don't want to convolve the entire image for every source).

--- include/lsst/meas/extensions/convolved/convolved.h
+++ include/lsst/meas/extensions/convolved/convolved.h
@@ -11,13 +11,25 @@ namespace lsst { namespace meas { namespace extensions { namespace convolved {
  */
 class ConvolvedFluxControl : public algorithms::FluxControl {
 public:
+    // convolution
     LSST_CONTROL_FIELD(seeing, std::vector<double>, "list of target seeings (FWHM, pixels)");
-    LSST_CONTROL_FIELD(radius, std::vector<double>, "list of radii for aperture flux (pixels)");
     LSST_CONTROL_FIELD(kernelScale, double, "scaling factor of kernel sigma for kernel size");
+
+    // aperture flux
+    LSST_CONTROL_FIELD(radius, std::vector<double>, "list of radii for aperture flux (pixels)");
+
+    // Kron flux
+    LSST_CONTROL_FIELD(kronName, std::string, "base name of Kron fields in reference");
+    LSST_CONTROL_FIELD(maxSincRadius, double,
+                       "Largest aperture for which to use the sinc aperture code for Kron");
+    LSST_CONTROL_FIELD(kronRadiusForFlux, double, "Number of Kron radii for Kron flux");
 
     ConvolvedFluxControl() :
         algorithms::FluxControl("flux.convolved"),
-        kernelScale(4.0)
+        kernelScale(4.0),
+        kronName("flux.kron"),
+        maxSincRadius(10.0),
+        kronRadiusForFlux(2.5)
     {
         seeing.push_back(3.5);
         seeing.push_back(5.0);
